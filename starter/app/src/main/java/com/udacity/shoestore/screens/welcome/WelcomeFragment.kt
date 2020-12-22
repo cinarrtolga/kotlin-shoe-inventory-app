3ba43a0131f2a8b75a07ca595feeb53d08bfb74d
Tolga ÇINAR <cinarrtolga@gmail.com>
2020-12-22 11:52:48 +0000

    Welcome Fragment craeted and navigation completed

--- starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -1,16 +1,15 @@
-package com.udacity.shoestore.screens.login
+package com.udacity.shoestore.screens.welcome
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
-import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
-class LoginFragment : Fragment() {
-    private lateinit var binding: FragmentLoginBinding
+class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +19,7 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater)
-
-        binding.loginBtn.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
-        }
+        binding = FragmentWelcomeBinding.inflate(inflater)
 
         // Inflate the layout for this fragment
         return binding.root
